4d19630ed2951ae68b32de2aa4863e60b26bc921
vararais <varadigmaisnann@gmail.com>
2025-11-07 16:17:09 +0700

    Menyesuaikan spacing dengan di TV depan

--- app/src/main/java/com/example/questnavigasiui_049/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_049/view/Formulir.kt
@@ -94,7 +94,7 @@ fun FormIsian(
                 modifier = Modifier.fillMaxWidth(1f)
                     .padding(25.dp),
                 onClick = OnSubmitBtnClick
-            ) {
+            ){
                 Text(stringResource(id= R.string.submit))
             }
         }
