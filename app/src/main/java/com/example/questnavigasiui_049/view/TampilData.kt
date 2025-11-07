244c9b8fc253341f63a3ad8cc73f6d8e1c5c3ba0
vararais <varadigmaisnann@gmail.com>
2025-11-07 16:20:11 +0700

    Menambah Button

--- app/src/main/java/com/example/questnavigasiui_049/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_049/view/TampilData.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
@@ -53,6 +54,7 @@ fun TampilData(
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
                 Spacer(modifier = Modifier.height(10.dp))
+                Button()
             }
         }
 
